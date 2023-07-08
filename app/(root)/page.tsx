5c051d6397357f8bf719578a5e302013b7311209
jmonto55 <jmonto55@gmail.com>
2023-07-07 22:36:53 -0500

    Configure Modal and Zustand to manage state

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+import { isObjectBindingPattern } from "typescript";
+
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
   return (
     <div className="p-4">
-      <Button size="default" variant="default">Click Me</Button>
+       Root Page
     </div>
-  )
+  );
 }
+
+export default SetupPage;
